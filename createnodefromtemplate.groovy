e5a867669adb7dd3da7173c2fb7b297b1e58665c
Volker Boerchers <vboerchers@users.noreply.github.com>
2019-08-25 21:50:30 +0200

    strip template_prefix + optional whitespace at the front in the chooser

--- createnodefromtemplate.groovy
+++ createnodefromtemplate.groovy
@@ -1,12 +1,11 @@
 /* Template Chooser Script
 A modification of the original script by Quinbus Flestrin, December 12, 2011
-Modified by Allan at ADXSoft 20th Jan 2017
 
+Modified by Allan at ADXSoft 20th Jan 2017
 Purpose is to locate and copy pre-defined Templates as new branches to the selected node
 The script uses a GUI swingbuilder combo box to capture user input as to which template
 and then searches the map for any nodes with node text that commences with the string "Template:""
 (these templates can be anywhere in the map).
-
 
 Version 0.1
 
@@ -24,9 +23,8 @@ import javax.swing.JOptionPane
 template_prefix="Template:"
 
 // scan map for Templates
-templateList=[]
-node.map.root.find({it.text.startsWith(template_prefix)}).each{
-    templateList+=it.text
+templateList = node.map.root.find{ it.text.startsWith(template_prefix) }.collect{
+    it.text.replaceFirst("^${template_prefix}\\s*", '')
 }
 
 
@@ -34,7 +32,6 @@ def s = new SwingBuilder()
 s.setVariable('myDialog-properties',[:])
 def vars = s.variables
 def dial = s.dialog(title:'Template Picker', id:'myDialog', minimumSize: [300,50], modal:true, locationRelativeTo:ui.frame, owner:ui.frame, defaultCloseOperation:JFrame.DISPOSE_ON_CLOSE, pack:true, show:true) {
-
     panel() {
 
         boxLayout(axis:BXL.Y_AXIS)
@@ -53,24 +50,18 @@ def dial = s.dialog(title:'Template Picker', id:'myDialog', minimumSize: [300,50
         }
     }
 }
+
 if (vars.ok){
-
     c.find{
-        it.text == vars.type.selectedItem
+        it.text.matches(template_prefix + '\\s*' + vars.type.selectedItem)
     }.each {
-        def Copy = node.appendBranch(it)
-
+        def copy = node.appendBranch(it)
+        type = vars.type.selectedItem
+        copy.text = type
         // create attributes in the new node (comment these lines out if not required)
-        type=vars.type.selectedItem
-        plen=template_prefix.length()
-        type=type[plen+1..plen+type.length()-plen-1]
-        Copy.text=type
-        Copy.attributes.set("type", type)
-        def today = new Date()
-        def formattedDate = today.format('dd-MM-yy')
-        Copy.attributes.set("created", formattedDate)
+        copy.attributes.set("type", type)
+        copy.attributes.set("created", textUtils.defaultDateFormat.format(new Date()))
         // end of attributes creation
     }
 }
 //JOptionPane.showMessageDialog(ui.frame, 'REMINDER: Dates can be quickly inserted in each node with the Insert Date feature in property panel', "Template Picker" , JOptionPane.INFORMATION_MESSAGE);
-
